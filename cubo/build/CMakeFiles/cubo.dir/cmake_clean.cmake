b928b20dcf20ae44d7992a674bc920bd41181a2b
Luis Iraheta <liluisjose1@hotmail.com>
2018-03-07 16:32:07 -0600

    Cubo

---
+++ cubo/build/CMakeFiles/cubo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cubo.dir/cubo.cpp.o"
+  "cubo.pdb"
+  "cubo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cubo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
