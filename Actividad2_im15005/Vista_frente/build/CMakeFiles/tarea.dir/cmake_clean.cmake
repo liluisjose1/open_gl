5ebd46ae422fc4b5cd847b07d5e74d137c956c0a
Luis Iraheta <liluisjose1@hotmail.com>
2018-03-10 22:02:49 -0600

    subida2

---
+++ Actividad2_im15005/Vista_frente/build/CMakeFiles/tarea.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tarea.dir/gluLookAt.cpp.o"
+  "tarea.pdb"
+  "tarea"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tarea.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
