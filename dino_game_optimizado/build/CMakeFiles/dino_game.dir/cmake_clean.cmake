9864f7aa20eec805b186c870fa041626a4e8ad01
Luis Iraheta <liluisjose1@hotmail.com>
2018-06-01 11:58:09 -0600

    respaldo

---
+++ dino_game_optimizado/build/CMakeFiles/dino_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dino_game.dir/dino_optimizado.cpp.o"
+  "dino_game.pdb"
+  "dino_game"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dino_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
